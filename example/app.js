159b50219670bd5c06e884796da616e46f1701da
graforlock <maciej@neatcube.co.uk>
2016-10-21 16:16:43 +0100

    Vtree example refined 2

--- example/app.js
+++ example/app.js
@@ -1,48 +1,5 @@
 var router = require('../dist').router,
-    h = require('virtual-dom/h'),
-    diff = require('virtual-dom/diff'),
-    createElement = require('virtual-dom/create-element');
-
-
-var Component = {
-    state: { test: 0 },
-    app: document.getElementById('app'),
-    update: function (newValue)
-    {
-            Component.state = newValue;
-            Component.diffTree();
-    },
-    diffTree: function()
-    {
-        if(!this.tree)
-        {
-            this.tree = this.render();
-            this.init();
-        }
-        else
-        {
-            diff(this.tree, this.render());
-        }
-    },
-    init: function()
-    {
-        this.app.appendChild(this.tree);
-    },
-    subscribe: function (stream)
-    {
-        stream.onValue(function (value)
-        {
-            Component.update(value);
-        });
-    },
-    render: function()
-    {
-        return createElement(
-            h('h1',
-                {},
-                ['render! test state is ' + JSON.stringify(Component.state.test)]));
-    }
-};
+    Component = require('./component');
 
 document.querySelector('#test-3').addEventListener('click', function (e)
 {
