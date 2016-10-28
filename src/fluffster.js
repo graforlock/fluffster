d351bf0073c02dfba3d245accf37b2cdfd9faae4
graforlock <maciej@neatcube.co.uk>
2016-10-28 17:06:35 +0100

    Source annotations added

--- src/fluffster.js
+++ src/fluffster.js
@@ -3,16 +3,24 @@ var kefir = require('kefir');
 
 function State(setup, propertyKeepAlive$)
 {
+    /*  0. Constructor check */
+
     if (this instanceof State)
     {
-        this.state =  setup.appState;
+        /* 1. Inital setup */
 
+        this.state = setup.appState;
         this._localStream$ = kefir.pool();
         this._propertyKeepAlive$ = propertyKeepAlive$;
-
         this._component = setup.component;
-        if(setup.id) this._id = setup.id;
+        this.listeners = [];
+        if (setup.id) this._id = setup.id;
+        if (setup.messages) this.assignMessages(setup.messages);
 
+        /* 2. If there's two streams,
+           stream merge occurs. */
+
+        /* @Computed property */
         this._combinedStream$$ = this._localStream$ && this._propertyKeepAlive$
             ? kefir.combine([this._localStream$, this._propertyKeepAlive$], function (a, b)
             {
@@ -20,15 +28,13 @@ function State(setup, propertyKeepAlive$)
             })
             : false;
 
-        this.listeners = [];
-
-        if (setup.messages) this.assignMessages(setup.messages);
-
+        /* @Class method */
         this.onNext = function (state)
         {
             this._localStream$.plug(utils.emit(state));
         }.bind(this);
 
+        /* @Class method */
         this.updateState = function (newState)
         {
             var comparator = utils.extendMany({}, this.state, newState);
@@ -39,22 +45,34 @@ function State(setup, propertyKeepAlive$)
             }
         }.bind(this);
 
+        /* 3. Initialise program. */
+
+        /* @Initialisers */
         this.onNext(this.state);
         this.provide();
     }
     else
     {
+        /* Constructor check executed when no 'new'
+           present in the call. */
+
         return new State(setup, propertyKeepAlive$);
     }
 }
 
+
 State.prototype.stream = function ()
 {
+    /* Returns a stream or combined streams. */
+
     return this._combinedStream$$ ? this._combinedStream$$ : this._localStream$;
 };
 
+
 State.prototype.provide = function ()
 {
+    /* Provides the state to all components. */
+
     this.stream().onValue(function (state)
         {
             utils.each(this._component, function (component)
@@ -66,17 +84,25 @@ State.prototype.provide = function ()
     );
 };
 
+
 State.prototype.passMessage = function (message, payload)
 {
+    /* Passes a messages if a message interface
+       exists in the current context. */
+
     if (message in this.messages)
     {
         this.updateState(this.messages[message](this.state, payload));
     }
 };
 
+
 State.prototype.assignMessages = function (messages)
 {
+    /* Messages list setter. */
+
     this.messages = messages;
 };
+
 
 module.exports = State;
